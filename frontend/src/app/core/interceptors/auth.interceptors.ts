72eafd0b903b37993435eb2ac847a63bfc208918
Mankiran Singh <mankiransingh22@gmail.com>
2025-12-28 18:42:32 +0530

    feat: added interceptors

---
+++ frontend/src/app/core/interceptors/auth.interceptors.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpInterceptor,
+  HttpRequest,
+  HttpHandler
+} from '@angular/common/http';
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+  intercept(req: HttpRequest<any>, next: HttpHandler) {
+    return next.handle(
+      req.clone({
+        withCredentials: true //  REQUIRED for cookies
+      })
+    );
+  }
+}
