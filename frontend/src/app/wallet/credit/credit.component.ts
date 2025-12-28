978c8bbbb2b9d4ac4d5db0dc3160a393782727cf
Mankiran Singh <mankiransingh22@gmail.com>
2025-12-28 20:38:00 +0530

    feat: statement component

--- frontend/src/app/wallet/credit/credit.component.ts
+++ frontend/src/app/wallet/credit/credit.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import { WalletService } from '../../core/services/wallet.service';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatCardModule } from '@angular/material/card';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-credit',
   standalone: true,
-  imports: [],
+  imports: [MatFormFieldModule, MatCardModule, FormsModule],
   templateUrl: './credit.component.html',
   styleUrl: './credit.component.scss'
 })
 export class CreditComponent {
+ childId = '';
+  amount!: number;
 
+  constructor(private wallet: WalletService) {}
+
+  credit() {
+    this.wallet.credit({
+      childId: this.childId,
+      amount: this.amount
+    }).subscribe({
+      next: () => alert('Credit successful'),
+      error: err => alert(err.error?.error || err.error)
+    });
+  }
 }
--- frontend/src/app/wallet/statement/statement.component.ts
+++ frontend/src/app/wallet/statement/statement.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { WalletService } from '../../core/services/wallet.service';
+import { MatCardModule } from '@angular/material/card';
+import { MatTableModule } from '@angular/material/table';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-statement',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, MatTableModule, DatePipe],
   templateUrl: './statement.component.html',
   styleUrl: './statement.component.scss'
 })
 export class StatementComponent {
+transactions: any[] = [];
 
+  constructor(private wallet: WalletService) {}
+
+  ngOnInit() {
+    this.wallet.statement().subscribe((res: any) => {
+      this.transactions = res;
+    });
+  }
 }
