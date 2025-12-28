19778f79eafe6a4bf03ffa6bf7f3548c15ada44e
Mankiran Singh <mankiransingh22@gmail.com>
2025-12-28 20:32:51 +0530

    feat: added create user and downline

--- frontend/src/app/users/create-user/create-user.component.ts
+++ frontend/src/app/users/create-user/create-user.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
+import { UserService } from '../../core/services/user.service';
+import { MatFormField } from '@angular/material/form-field';
+import { MatCardModule } from '@angular/material/card';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-create-user',
   standalone: true,
-  imports: [],
+  imports: [MatFormField,MatCardModule, FormsModule],
   templateUrl: './create-user.component.html',
   styleUrl: './create-user.component.scss'
 })
 export class CreateUserComponent {
+name = '';
+  email = '';
+  password = '';
 
+  constructor(private userService: UserService) {}
+
+  createUser() {
+    this.userService.createUser({
+      name: this.name,
+      email: this.email,
+      password: this.password
+    }).subscribe({
+      next: () => alert('User created successfully'),
+      error: err => alert(err.error?.error || err.error)
+    });
+  }
 }
--- frontend/src/app/users/downline/downline.component.ts
+++ frontend/src/app/users/downline/downline.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
-
+import { UserService } from '../../core/services/user.service';
+import { MatCardModule } from '@angular/material/card';
+import { MatTableModule } from '@angular/material/table';
 @Component({
   selector: 'app-downline',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, MatTableModule],
   templateUrl: './downline.component.html',
   styleUrl: './downline.component.scss'
 })
 export class DownlineComponent {
+users: any[] = [];
 
+  constructor(private userService: UserService) {}
+
+  ngOnInit() {
+    this.userService.downline().subscribe((res: any) => {
+      this.users = res;
+    });
+  }
 }
