42e3b38be1fbe561ab433bbd7671313fa8a16069
Mankiran Singh <mankiransingh22@gmail.com>
2025-12-28 20:35:18 +0530

    feat: recharge component

--- frontend/src/app/wallet/recharge/recharge.component.ts
+++ frontend/src/app/wallet/recharge/recharge.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { WalletService } from '../../core/services/wallet.service';
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-recharge',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, MatFormFieldModule, MatCardModule],
   templateUrl: './recharge.component.html',
   styleUrl: './recharge.component.scss'
 })
 export class RechargeComponent {
+amount!: number;
 
+  constructor(private wallet: WalletService) {}
+
+  recharge() {
+    this.wallet.recharge(this.amount).subscribe({
+      next: () => alert('Recharge successful'),
+      error: err => alert(err.error?.error || err.error)
+    });
+  }
 }
